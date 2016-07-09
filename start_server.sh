b70a4088ad0e0a73c8acdce5e6e6f7cdb19e2f4a
Madhu Chakravarthy <ncmadhu@gmail.com>
2016-07-09 19:04:20 +0530

    Adding modified .travis.yml and start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 echo $TRAVIS_BUILD_DIR
-echo "export CODE_HOME=$TRAVIS_BUILD_DIR/FlaskDemo"
-echo $CODE_HOME
-echo "export PYTHONPATH=\$PYTHONPATH:\$CODE_HOME"
-python $TRAVIS_BUILD_DIR/FlaskDemo/api/apiserver.py
+echo "export PYTHONPATH=\$PYTHONPATH:\$TRAVIS_BUILD_DIR"
+python $TRAVIS_BUILD_DIR/api/apiserver.py
