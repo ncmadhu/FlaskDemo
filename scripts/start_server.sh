1ed7f074bcdc0d8ffbcb85b8eaa7955b3b4affed
Madhu Chakravarthy <ncmadhu@gmail.com>
2016-07-10 12:19:53 +0530

    Adding files for supervisord

---
+++ scripts/start_server.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+echo $REPO_HOME
+echo "export PYTHONPATH=\$PYTHONPATH:\$REPO_HOME"
--- start_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo $TRAVIS_BUILD_DIR
-echo "export PYTHONPATH=\$PYTHONPATH:\$TRAVIS_BUILD_DIR"
-python $TRAVIS_BUILD_DIR/api/apiserver.py
